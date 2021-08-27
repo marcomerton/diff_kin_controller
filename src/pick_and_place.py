965af8e6abb2558a694c841f2e6ba38f8bba9660
marcomerton <m.lepri2@studenti.unipi.it>
2021-08-27 16:05:14 +0200

    Renamed main file

--- src/pick_and_place.py
+++ src/pick_and_place.py
@@ -13,31 +13,47 @@ initial_orient = [[1, 0, 0],
                   [0, 0, 1]]
 '''
 
-def interpolate_point(p0, p1, t):
+def interpolate_point(t, p0, p1):
     ''''''
     return p1 * t + p0 * (1 - t)
 
+
 def slerp(t, P, Q):
     ''''''
-    diff = Q - P
-    theta = np.arccos(diff.angle)
+    cos_half_theta = P.w * Q.w + P.x * Q.x + P.y * Q.y + P.z * Q.z
+    if abs(cos_half_theta) >= 1:
+        return P
 
-    res = ( np.sin(theta/2*(1-t)) * P + np.sin(theta/2*t) * Q ) / np.sin(theta/2)
+    half_theta = np.arccos(cos_half_theta)
+
+    sin_half_theta = np.sqrt(1 - cos_half_theta * cos_half_theta)
+    ratioP = np.sin((1 - t) * half_theta) / sin_half_theta
+    ratioQ = np.sin(t * half_theta) / sin_half_theta
+
+    res = Quaternion()
+    res.w = P.w * ratioP + Q.w * ratioQ
+    res.x = P.x * ratioP + Q.x * ratioQ
+    res.y = P.y * ratioP + Q.y * ratioQ
+    res.z = P.z * ratioP + Q.z * ratioQ
+
     return res
 
 
-def move(start, end, nsteps, time):
+def move(start_p, start_o, end_p, end_o, nsteps, time):
 
     # Compute (constant) velocity
-    d = start - end
+    d = start_p - end_p
     tw = Twist(linear=Vector3(d[0], d[1], d[2]), angular=Vector3(0, 0, 0))
 
     # Start following trajectory
     rate = rospy.Rate(nsteps/time * 0.8)
-    for t in range(nsteps):
-        p = interpolate_point(start, end, (t+1)/nsteps)
+    for step in range(1, nsteps+1):
+        t = step/nsteps
 
-        pose = Pose(position = Point(*tuple(p)), orientation = Quaternion(0, 1, 0, 0))
+        p = interpolate_point(t, start_p, end_p)
+        q = slerp(t, start_o, end_o)
+
+        pose = Pose(position = Point(*tuple(p)), orientation = q)
 
         pub_pose.publish(pose)
         pub_vel.publish(tw)
@@ -62,10 +78,8 @@ if __name__ == "__main__":
     desired_vel_topic = rospy.get_param("desired_vel_topic")
     gripper_command_topic = rospy.get_param("gripper_command_topic")
 
-    # Initialize publisher for desired pose
+    # Initialize publishers
     pub_pose = rospy.Publisher(desired_pose_topic, Pose, queue_size=1)
-
-    # Initialize publisher for desired velocity
     pub_vel = rospy.Publisher(desired_vel_topic, Twist, queue_size=1)
 
     # Driver to control the gripper
@@ -75,17 +89,23 @@ if __name__ == "__main__":
     rospy.sleep(3)
 
 
-    pub_pose.publish(Pose(position = Point(0.7, 0.2, 0.7),
-                          orientation = Quaternion(0, 1, 0, 0)))
+    # Initial position and orientation
+    p0 = np.array([0.7, 0.2, 0.7])
+    q0 = Quaternion(0, 0, 0, 1)
+
+    # Pick position and orientation
+    p1 = np.array([0.5, -0.5, 0.4])
+    q1 = Quaternion(0, 1, 0, 0)
+
+    # Place position and orientation
+    p2 = np.array([-0.7, -0.2, 0.3])
+    q2 = Quaternion(1, 0, 0, 0)
 
 
-
-    p0 = np.array([0.7, 0.2, 0.7])      # Initial position
-    p1 = np.array([0.5, -0.5, 0.4])     # Pick position
-    p2 = np.array([-0.7, -0.2, 0.3])    # Place position
+    pub_pose.publish(Pose(position = Point(*tuple(p0)), orientation = q0))
 
 
-    move(p0, p1, 20, 5)
+    move(p0, q0, p1, q1, 20, 5)
 
 
     # == Pick up the cube == #
@@ -94,18 +114,22 @@ if __name__ == "__main__":
     #   2. close the gripper
     #   3. bring the arm up again in the original position
 
-    pub_pose.publish(Pose(position = Point(p1[0], p1[1], 0.19),
-                          orientation = Quaternion(0, 1, 0, 0))
-    )
+    pick_position = np.array([p1[0], p1[1], 0.19])
+
+    # pub_pose.publish(Pose(position = Point(p1[0], p1[1], 0.19),
+    #                       orientation = q1)
+    # )
+    move(p1, q1, pick_position, q1, 5, 1)
     rospy.sleep(2)
 
     gripper.close()
     rospy.sleep(2)
 
-    pub_pose.publish(Pose(position = Point(*tuple(p1)),
-                          orientation = Quaternion(0, 1, 0, 0))
-    )
+    # pub_pose.publish(Pose(position = Point(*tuple(p1)),
+    #                       orientation = q1)
+    # )
+    move(pick_position, q1, p1, q1, 5, 1)
     rospy.sleep(2)
 
 
-    move(p1, p2, 20, 10)
+    move(p1, q1, p2, q2, 20, 5)
